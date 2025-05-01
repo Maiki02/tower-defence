72bf546e152d2427592668403136695ef9487bdf
Maiki02 <miqueasdavidgentile@gmail.com>
2025-05-01 19:12:58 -0300

    Character base

---
+++ Assets/Scripts/Characters/CharacterBase.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour, IHealth, IDamageable
+{
+    public float MaxHealth { get; private set; } = 100f; // Salud máxima del personaje
+    public float CurrentHealth { get; private set; } // Salud actual del personaje
+
+    protected virtual void Awake()
+    {
+        // Inicializa la salud actual al valor máximo
+        CurrentHealth = MaxHealth;
+    }
+
+    public virtual void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Se llama cuando el personaje muere
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
