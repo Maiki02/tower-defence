61930d6ed3bb2b647f60afc7bdf5d22a5120320b
Maiki02 <miqueasdavidgentile@gmail.com>
2025-05-03 13:34:22 -0300

    Arma a distancia funciona

--- Assets/Scripts/Weapon/RangedWeapon.cs
+++ Assets/Scripts/Weapon/RangedWeapon.cs
@@ -2,17 +2,43 @@ using UnityEngine;
 
 
 public class RangedWeapon : WeaponBase
-{
-    private float lastAttackTime;
+{   
+    [SerializeField] private Camera cam;
 
     public override void Attack()
     {
-        if (Time.time - lastAttackTime < data.cooldown) return;
-        lastAttackTime = Time.time;
-        // Reproduce animación
-        GetComponent<Animator>().Play(data.attackAnimation.name);
-        // Saca un proyectil del pool o Instantiate
-        var proj = Instantiate(data.projectilePrefab, transform.position, Quaternion.identity);
-        proj.GetComponent<Projectile>().Initialize(data.damage, data.range);
+        //if (Time.time - lastAttackTime < data.cooldown) return;
+        //lastAttackTime = Time.time;
+
+    Debug.Log("Vamo a atacar con Ranged");
+        // Animación
+        // GetComponent<Animator>().Play(data.attackAnimation.name);
+
+        // Hitscan
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, data.range, damageableLayers))
+        {
+            Debug.Log("Hay damageable players");
+            hit.collider.GetComponent<IDamageable>()?.TakeDamage(data.damage);
+            // Aquí spawn pool de efecto de impacto en hit.point
+        }
+
+        // Pool de muzzle flash + sonido
+        SpawnMuzzleFlash();
+        PlayShotSound();
     }
+
+    private void SpawnMuzzleFlash()
+    {
+        // Spawn pool de efecto de disparo
+        // Instantiate(muzzleFlashPrefab, camera.transform.position, Quaternion.identity);
+    }
+
+    private void PlayShotSound()
+    {
+        // Spawn pool de sonido de disparo
+        // AudioSource.PlayClipAtPoint(shotSound, camera.transform.position);
+    }
+
+
 }
